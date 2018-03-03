55d89a664b9556673f27e9abf59e47c5e095e3ee
Iavor Diatchki <iavor.diatchki@gmail.com>
2018-03-02 16:29:50 -0800

    Split off globals into a separate module.

--- asm/proofs/AES_128_ENC_x4.hs
+++ asm/proofs/AES_128_ENC_x4.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Utils
+import Globals
 
 main :: IO ()
 main =
@@ -85,23 +86,6 @@ setupStack =
 
      return (p, ret)
 
-
--- XXX: we should lookup address from the ELF file.
--- For the time being, we hard-code the locations
-setupGlobals :: Spec Pre ()
-setupGlobals =
-  do let size = 0x403000
-     reg <- allocBytes "globals" Immutable (size .* Byte)
-     registerRegion 0 reg
-
-     let declare _nm offset ty xs =
-           do () <- return (const () (_nm :: String)) -- tmp, to avoid warn
-              ptr <- ptrAdd reg (offset .* Byte)
-              writeMem ptr =<< mapM (literalAt ty) xs
-
-     declare "con1"     0x402ba0 DWord [ 1, 1, 1, 1 ]
-     declare "one"      0x402bd0 DWord [ 1, 0, 0, 0 ]
-     declare "and_mask" 0x402be0 DWord [ 0,0xffffffff, 0xffffffff, 0xffffffff ]
 
 
 

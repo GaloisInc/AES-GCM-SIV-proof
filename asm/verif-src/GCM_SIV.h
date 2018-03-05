79256cc2832c9593f4de060b88d6b0effa37a225
Iavor Diatchki <iavor.diatchki@gmail.com>
2018-03-05 11:14:12 -0800

    Cleanup warnings, add declarations for all functions.

--- asm/verif-src/GCM_SIV.h
+++ asm/verif-src/GCM_SIV.h
@@ -74,68 +74,141 @@
 
 
 typedef struct KEY_SCHEDULE
-	{
-		ALIGN64 unsigned char KEY[16*15];
-		unsigned int nr;
-	} ROUND_KEYS;
-	
+  {
+    ALIGN64 unsigned char KEY[16*15];
+    unsigned int nr;
+  } ROUND_KEYS;
+
 typedef struct GCM_SIV_CONTEXT
 {
-	ROUND_KEYS KS;
-	uint8_t Htbl[16*8];
-	ALIGN64 uint8_t secureBuffer[16*16];
-	#ifdef DETAILS
-	uint8_t details_info[16*56]; // first 20*16 for enc , last 20*16 for dec
-	#endif
+  ROUND_KEYS KS;
+  uint8_t Htbl[16*8];
+  ALIGN64 uint8_t secureBuffer[16*16];
+  #ifdef DETAILS
+  uint8_t details_info[16*56]; // first 20*16 for enc , last 20*16 for dec
+  #endif
 }AES_GCM_SIV_CONTEXT;
 
 
-void AES_GCM_SIV_Encrypt (AES_GCM_SIV_CONTEXT* ctx, uint8_t* CT, uint8_t* TAG, const uint8_t* AAD, const uint8_t* PT, size_t L1, size_t L2, const uint8_t* IV, const uint8_t* KEY);
+void AES_GCM_SIV_Encrypt
+  ( AES_GCM_SIV_CONTEXT* ctx
+  , uint8_t* CT
+  , uint8_t* TAG
+  , const uint8_t* AAD
+  , const uint8_t* PT
+  , size_t L1
+  , size_t L2
+  , const uint8_t* IV
+  , const uint8_t* KEY
+  );
 
-int AES_GCM_SIV_Decrypt (AES_GCM_SIV_CONTEXT* ctx, uint8_t* DT, uint8_t* TAG, const uint8_t* AAD, const uint8_t* CT, size_t L1, size_t L2, const uint8_t* IV, const uint8_t* KEY);
+int AES_GCM_SIV_Decrypt
+  ( AES_GCM_SIV_CONTEXT* ctx
+  , uint8_t* DT
+  , uint8_t* TAG
+  , const uint8_t* AAD
+  , const uint8_t* CT
+  , size_t L1
+  , size_t L2
+  , const uint8_t* IV
+  , const uint8_t* KEY
+  );
 
-void Polyval_Horner(unsigned char T[16],  			// input/output 
-					 unsigned char* H,				// H
-					 unsigned char* BUF,			// Buffer
-					 unsigned int blocks);			// LEn2
-					 
-void ENC_MSG_x4(unsigned char* PT, 
-				  unsigned char* CT, 				//Output
-				  unsigned char* TAG, 
-				  unsigned char* KS,
-				  int byte_len);
-				  
-void ENC_MSG_x8(unsigned char* PT, 
-				  unsigned char* CT, 				//Output
-				  unsigned char* TAG, 
-				  unsigned char* KS,
-				  int byte_len);
-				  
+void Polyval_Horner
+  ( unsigned char T[16]       // input/output
+  , const unsigned char* H    // H
+  , const unsigned char* BUF  // Buffer
+  , unsigned int blocks       // Len2
+  );
+
+
+void Polyval_Horner_AAD_MSG_LENBLK
+  ( uint8_t T[16]
+  , uint8_t Record_Hash_Key[16]
+  , const uint8_t *AAD
+  , uint64_t L1
+  , const uint8_t* PT
+  , uint64_t L2
+  , uint64_t len_blk[2]
+  );
+
+void ENC_MSG_x4
+  ( const unsigned char* PT
+  , unsigned char* CT     //Output
+  , unsigned char* TAG
+  , unsigned char* KS
+  , int byte_len
+  );
+
+void ENC_MSG_x8
+  ( const unsigned char* PT
+  , unsigned char* CT         //Output
+  , const unsigned char* TAG
+  , const unsigned char* KS
+  , int byte_len
+  );
+
 void INIT_Htable(uint8_t Htbl[16*8], uint8_t *H);
-void Polyval_Htable(uint8_t Htbl[16*8], uint8_t *MSG, uint64_t LEN, uint8_t T[16]);
 
-void Decrypt_Htable(unsigned char* CT, 				//input
-				 unsigned char* PT,					//output
-				 unsigned char POLYVAL_dec[16], 	//input/output
-				 unsigned char TAG[16],
-				 unsigned char Htable[16*8],
-				 unsigned char* KS, 				//Key Schedule for decryption
-				 int byte_len,
-				 unsigned char secureBuffer[16*8]);			
+void Polyval_Htable
+  ( uint8_t Htbl[16*8]
+  , const uint8_t *MSG
+  , uint64_t LEN
+  , uint8_t T[16]
+  );
+
+void Decrypt_Htable
+  ( const unsigned char* CT         //input
+  , unsigned char* PT               //output
+  , unsigned char POLYVAL_dec[16]   //input/output
+  , unsigned char TAG[16]
+  , unsigned char Htable[16*8]
+  , unsigned char* KS               //Key Schedule for decryption
+  , int byte_len
+  , unsigned char secureBuffer[16*8]
+  );
+
 /*
-	This function key expansion (128bit) first key - and encrypt 4 blocks in the following way:
-	CT[0] = AES_128(first_key, NONCE[95:0] || 0)
-	CT[1] = AES_128(first_key, NONCE[95:0] || 1)
-	CT[2] = AES_128(first_key, NONCE[95:0] || 2)
-	CT[3] = AES_128(first_key, NONCE[95:0] || 3)
+  This function key expansion (128bit) first key - and encrypt 4 blocks in the following way:
+  CT[0] = AES_128(first_key, NONCE[95:0] || 0)
+  CT[1] = AES_128(first_key, NONCE[95:0] || 1)
+  CT[2] = AES_128(first_key, NONCE[95:0] || 2)
+  CT[3] = AES_128(first_key, NONCE[95:0] || 3)
 */
-void AES128_KS_ENC_x1_INIT_x4(const unsigned char* NONCE, unsigned char* CT, unsigned char* KS,
-				   unsigned char* first_key);				 
-	
+void AES128_KS_ENC_x1_INIT_x4
+  ( const unsigned char* NONCE
+  , unsigned char* CT
+  , unsigned char* KS
+  , const unsigned char* first_key
+  );
 
-void AES_KS(unsigned char* key, unsigned char* KS);
-void AES_KS_ENC_x1(unsigned char* PT, unsigned char* CT, int len, unsigned char *KS, unsigned char* key);
-void INIT_Htable_6(unsigned char* Htbl, unsigned char* H);
-int Finalize_Tag(unsigned char* PT, unsigned char* CT, unsigned char* KS, uint8_t* orig_tag);
+
+void AES_KS(const unsigned char* key, unsigned char* KS);
+
+void AES_KS_ENC_x1
+  ( unsigned char* PT
+  , unsigned char* CT
+  , int len
+  , unsigned char *KS
+  , unsigned char* key
+  );
+
+void AES_128_ENC_x4
+  ( uint8_t *IV
+  , unsigned char* PT
+  , unsigned char* key
+  );
+
+
+
+void INIT_Htable_6 (unsigned char* Htbl , unsigned char* H);
+
+int Finalize_Tag
+  ( unsigned char* PT
+  , unsigned char* CT
+  , unsigned char* KS
+  , uint8_t* orig_tag
+  );
+
 void Clear_SIV_CTX(AES_GCM_SIV_CONTEXT* ctx);
 #endif
--- asm/verif-src/main.c
+++ asm/verif-src/main.c
@@ -2,7 +2,10 @@
 /* This is just a wrapper, so that we have all funcionts linked in
    a single executable */
 
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
+  (void) argc;
+  (void) argv;
+
   AES_GCM_SIV_Encrypt(0,0,0,0,0,0,0,0,0);
   AES_GCM_SIV_Decrypt(0,0,0,0,0,0,0,0,0);
   return 0;
